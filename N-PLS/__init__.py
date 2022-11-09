d165f7658b4386fa13622a1e004760ac0a3c3a54
89605502155 <ferubko1999@yandex.ru>
2022-11-10 02:59:27 +0300

    Create directory

---
+++ N-PLS/__init__.py
@@ -0,0 +1,5 @@
+from .n_pls import (N_PLS)
+
+__author__ = 'Andrey Ferubko and Ivan Krylov'
+__version__ = '1.0.0'
+__email__ = 'ferubko1999@yandex.ru'
---
+++ setup.py
@@ -0,0 +1,44 @@
+from io import open
+from setuptools import setup
+
+
+version = '1.0.0'
+
+with open('README.md', encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='N-PLS',
+    version=version,
+
+    author='Andrey Ferubko and Ivan Krylov',
+    author_email='ferubko1999@yandex.ru',
+
+    description=(
+        'Some text'
+    ),
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+
+    url='https://github.com/89605502155/N-PLS',
+    download_url='https://github.com/89605502155/N-PLS/archive/main.zip',
+
+    license='GNU General Public License v3.0',
+
+    packages=['N-PLS'],
+    install_requires=['numpy', 'sklearn'],
+
+    classifiers=[
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Operating System :: OS Independent',
+        'Intended Audience :: End Users/Desktop',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: Implementation :: PyPy',
+        'Programming Language :: Python :: Implementation :: CPython',
+    ]
+)
